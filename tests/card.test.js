9c0cf5998611143394a759020fcbb2d00d2117c5
abk182 <abk1612@gmail.com>
2018-06-17 01:03:22 +0300

    тест Jest

--- tests/card.test.js
+++ tests/card.test.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
 import { Card } from '../game/components/card'
 
 test('Card test is working', () => {
-    const component = renderer.create(
-        <Card id={'01'} value={'H5'} handler={obj=>console.log(obj)}/>
+/*
+    const card = shallow(
+        <Card id={'01'} value={'H5'} handler={obj => console.log(obj)}/>
     )
+
+    expect(card.test()).toEqual('H5')
+*/
+    const component = renderer.create(
+        <Card id={'01'} value={'H5'} handler={obj => console.log(obj)}/>
+    )
+
     let tree = component.toJSON()
     expect(tree).toMatchSnapshot()
-
     tree.props.onClick()
 
     tree = component.toJSON()
